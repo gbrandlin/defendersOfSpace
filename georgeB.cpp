630d32265025a663b2e6df01d558119dec66acb5
gbrandlin <32801034+gbrandlin@users.noreply.github.com>
2018-02-22 09:28:54 -0800

    Add files via upload
    george file 2/20/18

---
+++ georgeB.cpp
@@ -0,0 +1,25 @@
+//George Brandlin 
+//2/22/18
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+using namespace std;
+
+
+
+void bulletsound(){
+
+    cout << '\a';
+   // cout << "where is the sound?";
+
+}
+
+void PowerUp() {
+
+    //this function will generate at random a power pack allowing for more bullets to shoot out or making bullets bigger
+
+//Gives X ammount of power bullets to be used, will show a counter of how many are left to use
+
+}
+
+
