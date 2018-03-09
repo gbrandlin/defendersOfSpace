57d8002811b9370ec7b06b75dc30c40ff9224584
Student <student@lab-315-024.cs.csubak.edu>
2018-03-09 13:16:47 -0800

    small change

--- heribertoJ.cpp
+++ heribertoJ.cpp
@@ -1,42 +1,25 @@
 //Heriberto Jimenez
 //DoS Project
+//Last Modified 3/9/18
 
+#include "fonts.h"
+#include "GL/glx.h"
+#include <ctime>
 
-/*
-void physics()
+//-----------------------------------------------------------------------------
+//setup timers
+extern double timeDiff(struct timespec *start, struct timespec *end);
+extern void timeCopy(struct timespec *dest, struct timespec *source); 
+
+//----------------------------------------------------------------------------
+
+//print name and time functions
+void showNameheriberto()
 {
-if (gl.keys[XK_p]) {
-                 //a little time between each bullet
-                 struct timespec bt;
-                 clock_gettime(CLOCK_REALTIME, &bt);
-                 double ts = timeDiff(&g.bulletTimer, &bt);
-                 if (ts > 0.00005) {
-                         timeCopy(&g.bulletTimer, &bt);
-                         if (g.nbullets < MAX_BULLETS) {
-                                 //shoot a bullet...
-                                 //Bullet *b = new Bullet;
-                                 Bullet *b = &g.barr[g.nbullets];
-                                 timeCopy(&b->time, &bt);
-                                 b->pos[0] = g.ship.pos[0];
-                                 b->pos[1] = g.ship.pos[1];
-                                 b->vel[0] = g.ship.vel[0];
-                                 b->vel[1] = g.ship.vel[1];
-                                 //convert ship angle to radians
-                                 Flt rad = ((g.ship.angle+90.0) / 360.0f) * PI * 2.0;
-                                 //convert angle to a vector
-                                 Flt xdir = cos(rad);
-                                 Flt ydir = sin(rad);
-                                 b->pos[0] += xdir*20.0f;
-                                 b->pos[1] += ydir*20.0f;
-                                 b->vel[0] += xdir*6.0f + rnd()*0.1;
-                                 b->vel[1] += ydir*6.0f + rnd()*0.1;
-                                 b->color[0] = 1.0f;
-                                 b->color[1] = 1.0f;
-                                 b->color[2] = 1.0f;
-                                 g.nbullets++;
-                         }
-                 }
-         }
+    //blank
+    //
 
 }
-*/
+
+
+
